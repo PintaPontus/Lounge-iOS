88e0f4facd507a70b32da76ec4a9b20897ca6634
pintapontus <matteo.blascoviz@gmail.com>
2023-10-30 20:02:15 +0100

    Initial Commit

---
+++ Lounge/LoungeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoungeApp.swift
+//  Lounge
+//
+//  Created by Matteo Pintus on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LoungeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
