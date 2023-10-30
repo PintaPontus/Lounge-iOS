675db46b45e536151877ddc256372d9749bfb48a
pintapontus <matteo.blascoviz@gmail.com>
2023-10-30 20:08:20 +0100

    setup

--- Lounge/ContentView.swift
+++ Lounge/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "sofa")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Welcome on Lounge!")
         }
         .padding()
     }
